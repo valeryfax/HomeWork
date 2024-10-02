4b055db3bf3b4dacb11857db92184cad4b35457d
valeryfax <Valeryfax@mail.ru>
2024-10-02 10:47:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#1st program
+print((9**0.5)*5)
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#4st program
+name='123.456'
+name1=(int(float(name)*10)%10)
+print(name1)
