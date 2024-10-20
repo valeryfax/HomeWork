f469937ba49a639a9604edeb6fd3af0b8c5c5104
valeryfax <Valeryfax@mail.ru>
2024-10-20 22:32:30 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,30 @@
+def count_calls():
+    global calls
+    calls = calls + 1
+
+
+def string_info():
+    global stroka
+    print(f' {len(stroka)}, {stroka.upper()}, {stroka.lower()}')
+    count_calls()
+
+
+def is_contains():
+    global string
+    count_calls()
+    list_to_search = ["ПЕРВЫЙ", "ВТОРОЙ", "ТРЕТИЙ"]
+    if string in list_to_search:
+        n = True
+    else:
+        n = False
+    print(n)
+
+calls = 0
+while 1>0:
+    stroka = input("Введите строку: ")
+    string = (input("Введите строку: ").upper())
+    string_info ()
+
+    is_contains()
+
+    print(calls)
