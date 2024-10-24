584a3734c3d0d06d80ef06346e9b03061c5e37df
valeryfax <Valeryfax@mail.ru>
2024-10-24 17:09:33 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,22 +1,12 @@
-
-# def single_root_words (root_word, *other_words):
-#     same_words = []
-#     upper.slovo = root_word.
-#
-#     for slovo in other_words:
-#         if root_word in slovo or slovo in root_word:
-#             same_words.append(slovo)
-#     print(same_words)
-
 def single_root_words(root_word, *other_words):
     same_words = []
 
     upper_word = root_word.upper()
     for word in other_words:
-        if upper_word  in  word.upper():
+        if upper_word  in  word.upper() or word.upper() in upper_word :
             same_words.append(word)
     print(same_words)
-     
+
 
 
 
@@ -26,11 +16,3 @@ def single_root_words(root_word, *other_words):
 
 single_root_words( 'rich' , 'richiest', 'orichalcum', 'cheers', 'richies')
 single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-
-
-
-
-
-
-
-
