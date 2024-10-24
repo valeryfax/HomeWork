24af256a93b68898d953bf2c1abfa3fc677030ff
valeryfax <Valeryfax@mail.ru>
2024-10-24 22:26:32 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,10 +1,9 @@
 def single_root_words(root_word, *other_words):
     same_words = []
 
-    upper_word = root_word.upper()
-    for word in other_words:
-        if upper_word  in  word.upper() or word.upper() in upper_word :
-            same_words.append(word)
+    for slovo in other_words:
+        if root_word.upper()  in  slovo.upper() or slovo.upper() in root_word.upper():
+            same_words.append(slovo)
     print(same_words)
 
 
