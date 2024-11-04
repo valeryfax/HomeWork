5fda2a847eed2c5b750d2dfe75c4555e778ae2d4
valeryfax <Valeryfax@mail.ru>
2024-11-04 09:23:31 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,52 +1,26 @@
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-def perebor ():
-    spisok = []
-    for a in data_structure:
-        if isinstance(a, list):
-            for a1 in a:
-                spisok.append(a1)
-        if isinstance(a, dict):
-            for b1 in a.keys():
-                spisok.append (len(b1))
-            for b1 in a.values():
-                spisok.append (b1)
-        if isinstance(a, tuple):
-            for c1 in a:
-                if isinstance(c1, int):
-                    spisok.append(c1)
-                if isinstance(c1, dict):
-                    for c2 in c1.keys():
-                        spisok.append (len(c2))
-                    for c2 in c1.values():
-                        spisok.append (c2)
-                
-        if isinstance(a, str):
-            spisok.append (len(a))
-    for c3 in a:
-        for c4 in c3:
-            for c5 in c4:
-                for c6 in c5:
-                    if isinstance(c6, int):
-                        spisok.append(c6)
-                    if isinstance(c6, str):
-                        spisok.append (len(c6))    
-                    if isinstance(c6, tuple):
-                        for c7 in c6:
-                            if isinstance(c7, str):
-                                spisok.append (len(c7))
-                            if isinstance(c7, int):
-                                spisok.append(c7)
-    print(sum(spisok))
-    
-perebor()
+
+def calculate_structure_sum(*args):
+    all_summ = 0
+    for i in args:
+        if isinstance(i, (list, tuple, set)):
+            all_summ += calculate_structure_sum(*i)
+        elif isinstance(i, dict):
+            all_summ += calculate_structure_sum(*i.items())
+        elif isinstance(i, str):
+            all_summ += len(i)
+        elif isinstance(i, (int, float)):
+            all_summ += i
+    return all_summ
 
 
+result = calculate_structure_sum(data_structure)
 
-
+print(result)
